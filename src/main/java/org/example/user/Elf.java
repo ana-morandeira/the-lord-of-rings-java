fbb289879d0d6c3bb7c70fd52c7b633159cc61b1
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-02-24 20:49:27 +0100

    feature/ Elf.java

---
+++ src/main/java/org/example/user/Elf.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Elf extends Character {
+    private String magicType;
+
+    public Elf(String name, int age, int healthLevel, String mainWeapon, String magicType) {
+        super(name, age, healthLevel, mainWeapon);
+        this.magicType = magicType;
+    }
+
+    public String getMagicType() { return magicType; }
+    public void setMagicType(String magicType) { this.magicType = magicType; }
+
+    @Override
+    public void showDetails() {
+        super.showDetails();
+        System.out.println("Tipo de Magia: " + magicType);
+    }
+}
